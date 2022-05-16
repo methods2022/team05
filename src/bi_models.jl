5a2e96edec95ae1057318283fdc671814dcba1d6
Nasheath Ahmed <nasheath_ahmed@brown.edu>
2022-05-15 22:07:37 -0400

    Updated a file

--- src/bi_models.jl
+++ src/bi_models.jl
@@ -32,6 +32,32 @@ function svm_model()
 
 
 end
+# df = CSV.read("./final.csv", DataFrame)
+# targets = ["Fetus with unknown complication", "Tubal pregnancy", "Miscarriage in first trimester", "Preeclampsia", "Normal pregnancy"]
+
+# X = select(df, Not(["PATIENT_ID","BIRTHDATE","DEATHDATE","Fetus with unknown complication", "Tubal pregnancy", "Miscarriage in first trimester", "Preeclampsia", "Normal pregnancy"]))
+# for name in targets
+#     y = Array([])
+#     println(name)
+#     for row in eachrow(df)
+#         if row[name] == 1
+#             push!(y, 1)
+#             continue
+#         else
+#             push!(y, 0)
+#         end
+#     end
+#     y = categorical(y)
+#     train, test = partition(eachindex(y), 0.8)
+#     Tree = @load DecisionTreeClassifier() pkg=DecisionTree 
+#     tree_model = Tree(max_depth=4)
+#     tree = machine(tree_model, X, y)
+#     fit!(tree, rows=train)
+#     yhat = predict(tree, X[test,:])
+#     acc = accuracy(mode.(yhat), y[test])
+#     println("Test Set Accuracy on $(name): $acc")
+# end
+
 
 
 svm_model()
